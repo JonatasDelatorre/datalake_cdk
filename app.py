a4216432e49362c63bdfe9737308ab7b22a1aede
Jonatas <jonatas.delatorre@e-core.com>
2025-01-28 15:59:28 -0300

    deleting old files not used

--- app.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import aws_cdk as cdk
-from ddk_app.ddk_app_stack import DdkApplicationStack
-
-app = cdk.App()
-DdkApplicationStack(app, "DdkApplicationStack", "dev")
-
-app.synth()
